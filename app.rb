689830d4f97a03d343df34fa290d57b2531b3249
surpreet.mundra <surpreet.mundra@gmail.com>
2019-06-13 15:20:57 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -9,8 +9,8 @@ class App < Sinatra::Base
   end
 
   get "/square/:number" do
-    @square = params[:number].to_i * params[:number].to_i
-    @square.to_s
+    square = params[:number].to_i * params[:number].to_i
+    square.to_s
   end
 
   get "/say/:number/:phrase" do
