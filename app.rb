5d26fff5dbfa20cc1eaa5223cfa5b71ab27fd4c1
surpreet.mundra <surpreet.mundra@gmail.com>
2019-06-13 15:39:06 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -29,13 +29,16 @@ class App < Sinatra::Base
 
   get "/:operation/:number1/:number2" do
     if params[:operation] == "add"
-        params[:number1].to_i + params[:number2].to_i
+        output = params[:number1].to_i + params[:number2].to_i
     elsif params[:operation] == "subtract"
-        params[:number1].to_i - params[:number2].to_i
+        output = params[:number1].to_i - params[:number2].to_i
     elsif params[:operation] == "multiply"
         params[:number1].to_i * params[:number2].to_i
-    else params[:number1].to_i / params[:number2].to_i
+    elsif params[:operation] == "divide"
+      output = params[:number1].to_i / params[:number2].to_i
+    else "Operation no recognised"
     end
+    output.to_s
   end
   
 
