e60c5decf2035e88b5068853ae8f259a291ede07
surpreet.mundra <surpreet.mundra@gmail.com>
2019-06-13 15:32:45 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -28,6 +28,10 @@ class App < Sinatra::Base
   end
 
   get "/:operation/:number1/:number2" do
+    if params[:operation] = "add",
+        params[:number1] + params[:number2]
+      else "check"
+      end 
   end
 
 
